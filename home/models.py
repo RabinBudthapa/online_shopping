3fb49f751724fd9bf2d5f18d727d596b275dd1ed
Rabin Budthapa <btrabin@Rabins-MacBook-Pro.local>
2024-05-16 19:53:37 +0545

    models creating

--- home/models.py
+++ home/models.py
@@ -16,3 +16,33 @@ class Slider(models.Model):
     url=models.URLField(blank=True,max_length=500)
     def __str__(self):
        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='media')
+    description=models.TextField()
+    rank=models.ImageField()
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='media')
+    rank=models.ImageField()
+    def __str__(self):
+        return self.name
+STOCK=(('in_stock','In_Stock'),('out of stock','Out of Stock'))
+LABELS=(('','default'),('new','New'),('sale','Sale'),('hot','Hot'))
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    price=models.ImageField()
+    discounted_price=models.ImageField(default=0)
+    image=models.ImageField(upload_to='media')
+    description=models.TextField(blank=True)
+    specification=models.TextField(blank=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    brand=models.ForeignKey(Brand,on_delete=models.CASCADE)
+    stock = models.CharField(choices=STOCK,max_length=50)
+    labels=models.CharField(choices=LABELS,max_length=50)
+
+
